857c37277121c71cf72deebea4a67a3616937860
michal <micha0125@o2.pl>
2020-08-08 19:44:08 +0200

    Create new part of snake when we eat food

--- main.py
+++ main.py
@@ -13,18 +13,13 @@ class App:
         self.numberOfFood = 0
         self.food = []
         self.snakeParts = []
+        self.snakeMovement = 1
         self.snakeParts.append([self.x,self.y])
+        self.endPosition = [self.x,self.y]
         pyxel.run(self.update,self.draw)
     
     def update(self):
-        if self.direction == "N":
-            self.snakeUpdateMovement("N")
-        elif self.direction == "E":
-            self.snakeUpdateMovement("E")
-        elif self.direction == "S":
-            self.snakeUpdateMovement("S")
-        else:
-            self.snakeUpdateMovement("W")
+        self.snakeUpdateMovement(self.direction)
         self.movementCheck()
         self.checkCollisionSnakeWithFood()
         self.checkFoodAndUpdate()
@@ -32,16 +27,17 @@ class App:
     def snakeUpdateMovement(self,direction):
         if direction == "N":
             for i in self.snakeParts:
-                i[1] = (i[1] - 1) % pyxel.height
+                i[1] = (i[1] - self.snakeMovement) % pyxel.height
         elif direction == "E":
             for i in self.snakeParts:
-                i[0] = (i[0] + 1) % pyxel.width
+                i[0] = (i[0] + self.snakeMovement) % pyxel.width
         elif direction == "S":
             for i in self.snakeParts:
-                i[1] = (i[1] + 1) % pyxel.height
+                i[1] = (i[1] + self.snakeMovement) % pyxel.height
         else:
             for i in self.snakeParts:
-                i[0] = (i[0] - 1) % pyxel.width
+                i[0] = (i[0] - self.snakeMovement) % pyxel.width
+        self.endPosition = [self.snakeParts[-1][0],self.snakeParts[-1][1]]
 
     def collisionStatement(self,foodx,foody,snakeheadx,snakeheady,foodWidht,snakeWidht):
         if (foodx < snakeheadx + snakeWidht and 
@@ -49,12 +45,32 @@ class App:
         foody + foodWidht > snakeheady):
             return True 
         return False 
+
+    def createNewPartOfSnake(self):
+        if self.direction == "N":
+            newPartOfSnakeX = self.endPosition[0] 
+            newPartOfSnakeY = self.endPosition[1] + 8
+            self.snakeParts.append([newPartOfSnakeX, newPartOfSnakeY])
+        elif self.direction == "E":
+            newPartOfSnakeX = self.endPosition[0] + 8
+            newPartOfSnakeY = self.endPosition[1] 
+            self.snakeParts.append([newPartOfSnakeX, newPartOfSnakeY])
+        elif self.direction == "S":
+            newPartOfSnakeX = self.endPosition[0] 
+            newPartOfSnakeY = self.endPosition[1] - 8
+            self.snakeParts.append([newPartOfSnakeX, newPartOfSnakeY])
+        else :
+            newPartOfSnakeX = self.endPosition[0] - 8
+            newPartOfSnakeY = self.endPosition[1] 
+            self.snakeParts.append([newPartOfSnakeX, newPartOfSnakeY])
+
     def checkCollisionSnakeWithFood(self):
         count = 0
         for i in self.food:
             if self.collisionStatement(i[0],i[1],self.snakeParts[0][0],self.snakeParts[0][1],1.5,4):
                 del self.food[count]
                 print("Collision")
+                self.createNewPartOfSnake()
                 break
             count+=1
 
